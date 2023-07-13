6a1cb724c57b1796da87bc785183f1b2d2d6489b
504 <504@DESKTOP-G2B2I5O>
2023-07-13 17:20:56 +0900

    0713과제1답

---
+++ backend/src/과제/과제1/answar.java
@@ -0,0 +1,29 @@
+package 과제.과제1;
+
+public class answar {
+	/*	
+	 문제1. 4번
+	 문제2. 2,3,6,7번
+	 문제3. 정수타입 byte -> short,char ->int ->long
+	 		실수타입 float ->double
+	 		논리타입 boolean
+	 문제4.
+	 		타입 int		double
+	 	변수이름 age		price
+	 문제5. 3번
+	 문제6. 3번
+	 문제7. 6번
+	 문제8. 4번
+	 문제9. 2번
+	 문제10. 2번
+	 문제11. 13번줄 , 16번줄
+	 
+	 	
+	*/
+	
+	
+}
+	
+	
+	
+
