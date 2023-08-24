0387183a5e3301a0cd6ec601eb8dd5ee012e9faf
504 <504@DESKTOP-SFHCLDM>
2023-08-24 17:59:39 +0900

    0824

---
+++ backend/src/java2/day24_제네릭/Ex3_/백준.java
@@ -0,0 +1,13 @@
+package java2.day24_제네릭.Ex3_;
+
+import java.util.Scanner;
+
+public class 백준 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하시오1"); int ch =sc.nextInt();
+		System.out.println("숫자를 입력하시오2"); int ch1 =sc.nextInt();
+	
+	}
+}
